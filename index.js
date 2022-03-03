0e9d69cb782339c66884103dbac718d883b88d3b
xScoopy <69348168+xScoopy@users.noreply.github.com>
2022-03-02 18:53:14 -0600

    got json data to print to console

---
+++ index.js
@@ -0,0 +1,9 @@
+const strHelp = require('scoopy-str-helper')
+const dateHelp = require('datelibjs')
+
+const myData = require('./data.json')
+
+
+for (let i = 0; i < myData.length; i++){
+    console.log(myData[i])
+}
